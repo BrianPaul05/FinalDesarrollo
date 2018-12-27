daca811a40a1b6277eaa5a901a8de1b5dc5dbf98
Ronni <Ronni@DESKTOP-92IGCQP>
2018-12-27 11:13:18 -0500

    Cambios

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -8,16 +8,30 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
 
-    private Connection connect = null;
+    //private Connection connect = null;
+String url = "jdbc:postgresql://localhost:5432/viajes";
+    String user = "postgres";
+    String pwd = "1234";
+    Connection conectar = null;
 
     public Connection conexion() {
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection cn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "desarrollo", "desarrollo");
-            return cn;
-        } catch (ClassNotFoundException | SQLException | HeadlessException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
+            Class.forName("org.postgresql.Driver");
+            conectar = DriverManager.getConnection(url, user, pwd);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+        return conectar;
     }
+//    public Connection conexion() {
+//        try {
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            Connection cn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "desarrollo", "desarrollo");
+//            return cn;
+//        } catch (ClassNotFoundException | SQLException | HeadlessException ex) {
+//            JOptionPane.showMessageDialog(null, ex.getMessage());
+//        }
+//        return null;
+//    }
 }
