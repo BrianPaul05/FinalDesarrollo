daa46d7bff611ba3eafc8aa48000e3492d83bc77
BrianPaul05 <46172040+BrianPaul05@users.noreply.github.com>
2019-01-12 12:09:52 -0500

    Add files via upload

---
+++ src/Conexion/Conexion.java
@@ -0,0 +1,40 @@
+package Conexion;
+
+/**
+ *
+ * @author Roberto Altamirano
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    //private Connection connect = null;
+    String url = "jdbc:postgresql://localhost:5432/viajes";
+    String user = "postgres";
+    String pwd = "1234";
+    Connection conectar = null;//holakk
+
+    public Connection conexion() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conectar = DriverManager.getConnection(url, user, pwd);
+            //JOptionPane.showMessageDialog(null, "okk");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conectar;
+    }
+//    public Connection conexion() {
+//        try {
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            Connection cn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "desarrollo", "desarrollo");
+//            return cn;
+//        } catch (ClassNotFoundException | SQLException | HeadlessException ex) {
+//            JOptionPane.showMessageDialog(null, ex.getMessage());
+//        }
+//        return null;
+//    }
+}
